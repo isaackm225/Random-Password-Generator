6896d6f394c684733a012bc70575dcaa30cb4f82
Isaac K <95644163+Izy-stack@users.noreply.github.com>
2022-07-26 22:49:58 -0400

    Update UI.py

--- UI.py
+++ UI.py
@@ -1,14 +1,21 @@
-while True:
-    print("Please enter Y to enable any of these options.")
-    print("Or leave blank to disable.")
-    s=input("Add symboles? ")
-    d=input("Add digits? ")
-    l = input("Please enter the length of your password as an integer: ")
-    try:
-        l=int(l)
-    except:
-        print("Please enter a integer!!")
-    if type(l) is int:
-        return s,d,l
-    else:
-        print("Let's retry!")
+def ui()->tuple():
+    """Returns tuple of bools for symboles,digits and an int for len of pwd"""
+
+    while True:
+        print("Please enter Y to enable any of these options.")
+        print("Or leave blank to disable.")
+        s=bool(input("Add symboles? "))
+        d=bool(input("Add digits? "))
+        l=input("Please enter the length of your password as an integer: ")
+        try:
+            l=int(l)
+        except:
+            print("Please enter a integer!!")
+        if type(l) is int:
+            return s,d,l
+        else:
+            print("Let's retry!")
+            
+       
+
+    
