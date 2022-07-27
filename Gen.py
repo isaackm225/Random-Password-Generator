3f1038e84b1707a1d577eb767d88e496fb6049b3
Isaac K <95644163+Izy-stack@users.noreply.github.com>
2022-07-26 22:52:52 -0400

    Create Gen.py

---
+++ Gen.py
@@ -0,0 +1,38 @@
+import secret
+import string
+from UI import ui
+
+def Gen(s:bool, d:bool, l:int)->string:
+  """Returns a complicated string based on the param"""
+  
+  chars={
+  low: string.ascii_lowercase,
+  up: string.ascii_uppercase,
+  d:string.ascii_digits,
+  s:string.ascii_symboles
+  }
+  
+  symb=""
+  digit=""
+  up=""
+  low=""
+  
+  for i in 
+
+"""unpack param
+initialize the pool tuple
+add param if they =True
+initialize dict
+set default(var->const)
+
+for i in range(round(l/len(pool_tuple))):
+	for item in pool:
+		item.join(secrets.choice(dict[item]))"""
+
+
+
+if __name__==__main__:
+  
+  s,d,l=ui()
+
+p = " ".join()
